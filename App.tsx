02d189df8a82bcf10616765a3ad541be68bba9c9
awriyou <febriaryo@gmail.com>
2023-09-04 04:07:27 +0700

    Membuat component, functional component dan class component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 //import
-import React from 'react';
+import React, { Component } from 'react';
 import { Image, Text, TextInput, View } from "react-native";
 
 
@@ -18,6 +18,8 @@ const App = () => {
       
       <Photo />
       <TextInput style={{borderWidth: 1 }}/>
+      <BoxGreen/>
+      <Profile/>
     </View>
   ); 
 };
@@ -32,6 +34,26 @@ const Photo = () => {
   style={{ width: 100, height: 100 }}
   />;
 };
+
+
+//membuat component dengan class component, harus dengan capital
+class BoxGreen extends Component{
+  render(): React.ReactNode {
+      return <Text>Ini Component dari class component</Text>;
+  }
+}
+class Profile extends Component{
+  render(): React.ReactNode {
+      return (
+        <View>
+          <Image source={{uri: 'https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1936&q=80'  }} 
+            style={{ width: 100, height: 100 }} />
+          <Text style={{ color: 'blue', fontSize: 24 }}>Kucing</Text>
+        </View>
+      );
+  }
+}
+
 
 //output
 export default App;
